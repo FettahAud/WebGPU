c097871cf5002cc33ce7807b791bb72737bea817
fettahaud <fettahaud@gmail.com>
2023-09-26 23:46:37 +0300

    Drawing a recteangle

--- src/main.js
+++ src/main.js
@@ -17,8 +17,47 @@ context.configure({
   format: canvasFormat,
 });
 
-const vertices = new Float32Array([-0.8, -0.8, 0.8, -0.8, 0.8, 0.8, -0.8, 0.8]);
+const vertices = new Float32Array([
+  // Triangle 1
+  -0.8, -0.8, 0.8, -0.8, 0.8, 0.8,
 
+  // Triangle 2
+  -0.8, -0.8, 0.8, 0.8, -0.8, 0.8,
+]);
+
+const vertexBuffer = device.createBuffer({
+  label: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.Copy_DST,
+});
+
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset*/ 0, vertices);
+
+const vertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [
+    {
+      format: "float32x2",
+      offset: 0,
+      shaderLocation: 0, // Position
+    },
+  ],
+};
+
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: `
+    @vertex
+    fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+      return vec4f(pos, 0, 1); // (X, Y, Z, W)
+    }
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4f {
+      return vec4f(1, 0, 0, 1); // (Red, Green, Blue, Alpha) 
+    }
+  `,
+});
 const encoder = device.createCommandEncoder();
 
 const pass = encoder.beginRenderPass({
